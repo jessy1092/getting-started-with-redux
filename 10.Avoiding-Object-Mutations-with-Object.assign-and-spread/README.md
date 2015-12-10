472a27fad6d22a40097e1e3e6ef477fe07dd8baf
Lee <jessy1092@gmail.com>
2015-12-11 01:29:38 +0800

    feat: Update Section 10. Avoiding Object Mutations with Object.assign() and ...spread

---
+++ 10.Avoiding-Object-Mutations-with-Object.assign-and-spread/README.md
@@ -0,0 +1,16 @@
+Avoiding Object Mutations with Object.assign() and ...spread
+=============
+
+[Section 10](https://egghead.io/lessons/javascript-redux-avoiding-object-mutations-with-object-assign-and-spread)
+
+
+## Run
+
+```
+npm i
+```
+
+Then
+```
+npm start
+```
---
+++ 10.Avoiding-Object-Mutations-with-Object.assign-and-spread/index.js
@@ -0,0 +1,53 @@
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+// Not pure function
+// const toggleTodo = (todo) => {
+//   todo.completed = !todo.completed;
+//   return todo;
+// };
+
+const toggleTodo = (todo) => {
+  // ES5
+  // return {
+  //   id: todo.id,
+  //   text: todo.text,
+  //   completed: !todo.completed
+  // };
+
+  // ES6
+  // assign 的 object 有相同的 properties 時，會以之後的值為主。
+  // return Object.assign({}, todo, {
+  //   completed: !todo.completed
+  // });
+
+  // ES7
+  return {
+    ...todo,
+    completed: !todo.completed
+  };
+};
+
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  }
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+}
+
+testToggleTodo();
+console.log('All tests passed!');
