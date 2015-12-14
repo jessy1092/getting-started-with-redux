fc00550c5c6c3053fbb80fc9b593468ae3f8e9a4
Lee <jessy1092@gmail.com>
2015-12-15 01:21:28 +0800

    feat: Update Section 12. Writing a Todo List Reducer (Toggling a Todo)

---
+++ 12.Writing-a-Todo-List-Reducer(Toggling-a-Todo)/README.md
@@ -0,0 +1,16 @@
+Writing a Todo List Reducer (Toggling a Todo)
+=============
+
+[Section 12](https://egghead.io/lessons/javascript-redux-writing-a-todo-list-reducer-toggling-a-todo)
+
+
+## Run
+
+```
+npm i
+```
+
+Then
+```
+npm start
+```
---
+++ 12.Writing-a-Todo-List-Reducer(Toggling-a-Todo)/index.js
@@ -0,0 +1,97 @@
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    case 'TOGGLE_TODO':
+        return state.map(todo => {
+          // id 不相等時，直接回傳原本的 todo
+          if (todo.id !== action.id) {
+            return todo;
+          }
+          // id 相等時，回傳新的 todo 並改變 completed
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        });
+    default:
+      return state;
+  }
+}
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+}
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  // 改變 id 為 1 的 todo 的狀態 
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
+testAddTodo();
+testToggleTodo();
+console.log('All tests passed!');
