b8dc678407cd4e60657288e0f3a38a06faddab9c
Lee <jessy1092@gmail.com>
2015-12-16 00:59:31 +0800

    feat: Update Section 13. Reducer Composition with Arrays

---
+++ 13.Reducer-Composition-with-Arrays/README.md
@@ -0,0 +1,16 @@
+Reducer Composition with Arrays
+=============
+
+[Section 13](https://egghead.io/lessons/javascript-redux-reducer-composition-with-arrays)
+
+
+## Run
+
+```
+npm i
+```
+
+Then
+```
+npm start
+```
---
+++ 13.Reducer-Composition-with-Arrays/index.js
@@ -0,0 +1,107 @@
+
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+// Reducer 裡可以再使用更 basic reducer 來處理 state tree
+// todo reducer 處理單一 todo 的 state
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.txt,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+}
+
+// todos reducer 處理 todo array 並使用 todo reducer 處理單一 todo
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)  //  新增一個 todo
+      ];
+    case 'TOGGLE_TODO':
+        return state.map(t => todo(t, action));  //  更新 todo
+    default:
+      return state;
+  }
+}
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+}
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
+testAddTodo();
+testToggleTodo();
+console.log('All tests passed!');
